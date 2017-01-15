6a9c138699dc8987fdcdbd9e251bba5ce03044b1
John Fewell <fewell@gmail.com>
2017-01-15 21:40:00 +0000

    Added the base url and cities variables

--- lib/ra_events_cli_gem/cli.rb
+++ lib/ra_events_cli_gem/cli.rb
@@ -15,12 +15,12 @@ class RAEventsCliGem::CLI
     puts "+------------------------------------------------------------------------------------------------------+"
     puts "|1. London, UK        |2. Berlin, DE       |3. Paris, FR       |4. Switzerland   |5. New York, US      |"
     puts "|6. Amsterdam, NL     |7. Tokyo, JP        |8. Ibiza, ES       |9. Barcelona, ES |10. South + East, UK |"
-    puts "|11. Manchester, UK   |12. West + Wales, UK|13. Los Angeles, US|14. Ireland      |15. Miami, US        |"
-    puts "|16. San Francisco, US|17. Glasgow, UK     |18. Belgium        |19. Edinburgh, UK|20. Chubu, JP        |"
     puts "+------------------------------------------------------------------------------------------------------+"
   end
 
-  def todays_weather(input)
+  def list_dates(input)
+
+    puts ""
   end
 
   def ten_day_forecast
@@ -35,13 +35,13 @@ class RAEventsCliGem::CLI
   def menu
       input = nil
       while input != "exit"
-      puts "Choose a city to see current events there."
+      puts "Choose a city to see current events there. (type exit to quit)"
       input = gets.strip.downcase
       if input == "exit"
         exit
       else
-
-        todays_weather(input)
+        input.to_i!
+        list_dates(input)
       end
         puts "Do you want the 8 day forecast for this location? y/n"
         input = gets.strip.downcase
